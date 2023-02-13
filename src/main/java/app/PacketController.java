de6ecbecc7228dbcd70fb9e77888dde005fb3473
vladimir_lind_1 <lindermon@gmail.com>
2023-02-13 10:36:31 +0300

    first commit

--- src/main/java/app/PacketController.java
+++ src/main/java/app/PacketController.java
@@ -1,10 +1,10 @@
 package app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.concurrent.*;
 
 @Controller
-@EnableAsync
 @RequestMapping("/devices")
 public class PacketController {
+
+    private final DeviceRepository deviceRepository;
 
     //Пул потоков для ручных (инициированных пользователем) проверок
     private final ExecutorService userPool = Executors.newFixedThreadPool(10);
@@ -24,11 +25,13 @@ public class PacketController {
     //Пул потоков для проверок по расписанию
     private final ExecutorService scheduledPool = Executors.newFixedThreadPool(3);
 
-    // Спринг читает список "устройств" для регулярных проверок из application.properties
+    // Спринг читает список "устройств" из application.properties для регулярных проверок
     @Value("${devices}")
     private final String[] devices;
 
-    public PacketController(String[] devices) {
+    @Autowired
+    public PacketController(DeviceRepository deviceRepository, String[] devices) {
+        this.deviceRepository = deviceRepository;
         this.devices = devices;
     }
 
@@ -41,28 +44,29 @@ public class PacketController {
                 HttpStatus.OK);
     }
 
-/*    Запускаем задачу каждые 4 часа - проверяем в цикле полученный из application.properties
-    список "устройств" в потоках из выделенного пула потоков, чтобы не занимать пользовательский пул*/
+    /*    Запускаем задачу каждые 4 часа - проверяем в цикле полученный из application.properties
+        список "устройств" в потоках из выделенного пула потоков, чтобы не занимать пользовательский пул*/
     @Async
-    @Scheduled(initialDelay = 0, fixedRate = 4*60*60*1000)
-    public void checkPacketSizeBulkScheduled() throws InterruptedException, TimeoutException {
+    @Scheduled(initialDelay = 100000, fixedRate = 4 * 60 * 60 * 1000)
+    public void checkPacketSizeBulkScheduled() {
         for (String device : devices) {
             measurePackets(device, scheduledPool);
         }
     }
 
-    private DeviceDTO measurePackets(String ipAddress, ExecutorService executorService) {
-        Future<DeviceDTO> future = executorService.submit(new PacketMeasurer(ipAddress));
-        DeviceDTO deviceDTO;
+    private void measurePackets(String ipAddress, ExecutorService executorService) {
+        Future<Device> future = executorService.submit(new PacketMeasurer(ipAddress));
+        Device device;
         try {
-            deviceDTO = future.get(30, TimeUnit.SECONDS);
+            device = future.get(30, TimeUnit.SECONDS);
         } catch (Exception e) {
             e.printStackTrace();
             future.cancel(true);
             // Если в течение 30 секунд не вернулся результат проверки -
             // создаем дефолтный объект "устройства" с maxPacketSize = 0
-            return new DeviceDTO(ipAddress);
+            device = new Device(ipAddress, "0");
         }
-        return deviceDTO;
+        // Сохраняем результат в inmemory БД
+        deviceRepository.save(device);
     }
 }
--- src/main/java/app/PacketMeasurer.java
+++ src/main/java/app/PacketMeasurer.java
@@ -1,39 +1,60 @@
 package app;
 
-import org.apache.tomcat.util.file.Matcher;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 // Сервис проверки "устройства", создается каждый раз новый для каждой отдельной проверки с указанием
-// идентификатора "устройства" - в данном упрощении IP адрес.
-public class PacketMeasurer implements Callable<DeviceDTO> {
+// идентификатора "устройства" - в данном упрощении IP адрес. Может проводиться только одна проверка
+// для определенного "устройства" в момент времени.
+public class PacketMeasurer implements Callable<Device> {
 
-    Pattern packetSentRegex = Pattern.compile("получено\\s=\\s(\\w),");
-
-    private final Logger log = Logger.getLogger(PacketController.class.getName());
-
+    // Шаблон для парсинга ответа на ICMP запрос - 0 или 1 в group(1):
+    // Пакетов: отправлено = 1, получено = 1, потеряно = 0
+    Pattern packetSentRegex = Pattern.compile("\\s+получено\\s=\\s(\\w),");
     private final String deviceToCheck;
 
-    public PacketMeasurer(String ipAddress) {
-        this.deviceToCheck = ipAddress;
+    public PacketMeasurer(String deviceToCheck) {
+        this.deviceToCheck = deviceToCheck;
     }
 
     @Override
-    public DeviceDTO call() throws Exception {
-        sendPacketToDevice(1500);
-        log.info("device check executed for "+ deviceToCheck);
-        return new DeviceDTO();
+    public Device call() throws Exception {
+        // Синхронизация на "ID устройства" (айпи адресе) - только одна проверка в момент времени
+        // для одного устройства
+        synchronized (deviceToCheck) {
+            int maxPacketSize = 1600;
+            int minPacketSize = 0;
+            int tempPacketSize = maxPacketSize;
+            boolean isPassed;
+            // Бинарный поиск максимального размера ICMP пакета - 1600 байт желаемый размер
+            while (maxPacketSize - minPacketSize > 1) {
+                System.out.println("Temp: " + tempPacketSize + " Min: " + minPacketSize + ", Max: " + maxPacketSize);
+                isPassed = sendPacketToDevice(tempPacketSize);
+                if (isPassed) {
+                    minPacketSize = tempPacketSize;
+                } else {
+                    maxPacketSize = tempPacketSize;
+                }
+                tempPacketSize = (maxPacketSize + minPacketSize) / 2;
+            }
+            Device device = new Device(deviceToCheck, Integer.toString(minPacketSize));
+            System.out.println(Thread.currentThread().getName()+": "+device);
+            return device;
+        }
     }
 
 
-    private boolean sendPacketToDevice(Integer packetSize) throws IOException {
-        boolean isPassed = false;
+    private boolean sendPacketToDevice(int packetSize) throws IOException {
         long startTime = System.currentTimeMillis();
+        // Список, содержащий ответы от трех запросов пинг
+        List<String> results = new ArrayList<>(3);
+        // отправляем последовательно три пакета и проверяем, есть ли хотя бы один ответ из трех ICMP запросов
         for (int i = 0; i <= 2; i++) {
             String[] command = {"CMD", "/c", String.format("ping -n 1 -w 1 -l %s ", packetSize) + deviceToCheck};
             ProcessBuilder builder = new ProcessBuilder(command);
@@ -42,13 +63,19 @@ public class PacketMeasurer implements Callable<DeviceDTO> {
             BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream(), "Cp866"));
             String line;
             // Читаем и записываем данные, пока не истекли 3 секунды
-            while ((line = br.readLine()) != null && System.currentTimeMillis() - startTime > 3) {
+            while ((line = br.readLine()) != null) {
                 System.out.println(line);
-                if (packetSentRegex.matcher(line).matches()) {
-                    log.info("Packet sent: "+packetSentRegex.matcher(line).group(1));
+                Matcher matcher = packetSentRegex.matcher(line);
+                if (matcher.find()) {
+                    String result = matcher.group(1);
+                    System.out.println("Result: " + result);
+                    results.add(result);
+                }
+                if (System.currentTimeMillis() - startTime > 3000) {
+                    return false;
                 }
             }
         }
-        return isPassed;
+        return results.contains("1");
     }
 }
